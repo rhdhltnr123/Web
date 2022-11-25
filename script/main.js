e85680cf807209542dff53f06819cb65eb3d72ce
rhdhltnr123 <rhdhltnr123@naver.com>
2022-11-25 12:47:55 +0900

    2단계 메뉴 액션 및 효과

--- script/main.js
+++ script/main.js
@@ -1,4 +1,13 @@
 /* main.js */
 $(function () {
-  console.log("text");
+  $("nav .depth1 > li").hover(
+    function () {
+      // mouseover
+      $(this).children().addClass("show");
+    },
+    function () {
+      // mouseout
+      $(this).children().removeClass("show");
+    }
+  );
 });
